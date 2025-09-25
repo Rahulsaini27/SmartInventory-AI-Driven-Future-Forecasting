38bc1985cbf5f73ce4bcf3265e1e1de02ed3916a
Rahulsaini27 <rahulsaini42854@gmail.com>
2025-09-25 15:49:32 +0530

    updated env files for making secure api connection

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
   'http://localhost:5173', 
-  'https://your-frontend-domain.com'
+  'https://smart-inventory-ai-driven-future-fo-one.vercel.app'
 ];
 
 app.use(cors({
